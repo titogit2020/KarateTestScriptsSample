f508ab5dd9f39954305dab924f6d7364a3b38512
kaushik <kontact.kaushik@gmail.com>
2021-04-12 11:45:22 -0500

    initial commit

---
+++ src/main/java/Karate/KarateTestScriptsSample/App.java
@@ -0,0 +1,6 @@
+/*
+ * package Karate.KarateTestScriptsSample;
+ * 
+ * public class App { public static void main(String[] args) {
+ * System.out.println("Hello World!"); } }
+ */
---
+++ src/test/java/Karate/KarateTestScriptsSample/TestRunner.java
@@ -0,0 +1,17 @@
+package Karate.KarateTestScriptsSample;
+
+import javax.script.Bindings;
+
+import org.junit.runner.RunWith;
+
+import com.intuit.karate.junit4.Karate;
+
+@RunWith(Karate.class)
+public class TestRunner {
+
+	/*
+	 * @Override public Bindings createBindings() { return engine.createBindings();
+	 * }
+	 */
+
+}
